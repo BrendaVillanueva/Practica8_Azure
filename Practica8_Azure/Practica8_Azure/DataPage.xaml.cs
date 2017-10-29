91cdb5d292d9cde3ff84b5353bc5f1cd15511789
BRENDA <BRENDA@DESKTOP-7O4U7A5>
2017-10-29 00:42:50 -0500

    Agregar archivos de proyecto.

---
+++ Practica8_Azure/Practica8_Azure/DataPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Collections.ObjectModel;
+
+
+namespace Practica8_Azure
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataPage : ContentPage
+
+    {
+        public ObservableCollection<_13090416> Items { get; set; }
+        public static MobileServiceClient Cliente;
+        public static IMobileServiceTable<_13090416> Tabla;
+
+        public DataPage()
+        {
+            InitializeComponent();
+
+            Cliente = new MobileServiceClient(AzureConnection.AzureURL);
+            Tabla = Cliente.GetTable<_13090416>();
+            LeerTabla();
+            
+            
+        }
+
+        private void Insertar_Clicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new InsertPage());
+        }
+
+        async void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+            await Navigation.PushAsync(new SelectPage(e.SelectedItem as _13090416));
+        }
+
+        private async void LeerTabla()
+        {
+            IEnumerable<_13090416> elementos = await Tabla.ToEnumerableAsync();
+            Items = new ObservableCollection<_13090416>(elementos);
+            BindingContext = this;
+
+        }
+
+    }
+}
